f57f7b8dddc9cfd8f9bf689775455ac9b8016655
Ryan Walker <ryan.walker@infusionsoft.com>
2019-04-05 13:42:54 -0700

    add weeks

--- python/timeAtKubra.py
+++ python/timeAtKubra.py
@@ -1,9 +1,15 @@
 import datetime
+import math
 
 now = datetime.datetime.now().date()
 startDate = datetime.datetime(2019, 3, 18).date()
 
-daysAtKubra = abs((now - startDate).days)
+daysAtKubra = abs((now - startDate).days) + 1
 
-val = 'Days at Kubra: %s' % daysAtKubra
-print val
+days = 'Day %s' % daysAtKubra
+weeks = int(math.ceil(daysAtKubra / 7.0))
+weeksString = 'Week %s' % weeks
+
+print 'Time at KUBRA'
+print weeksString
+print days
