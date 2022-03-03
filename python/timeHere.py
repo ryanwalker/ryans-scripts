6ec0bc57e1dfd49c010cef48781b2ed61e862557
Ryan Walker <ryanwalker7@gmail.com>
2022-03-03 10:08:48 -0700

    renamed to timeHere.py

--- python/timeHere.py
+++ python/timeHere.py
@@ -2,7 +2,7 @@ import datetime
 import math
 
 now = datetime.datetime.now().date()
-startDate = datetime.datetime(2019, 3, 18).date()
+startDate = datetime.datetime(2022, 2, 28).date()
 
 daysAtKubra = abs((now - startDate).days) + 1
 
@@ -10,6 +10,6 @@ days = 'Day %s' % daysAtKubra
 weeks = int(math.ceil(daysAtKubra / 7.0))
 weeksString = 'Week %s' % weeks
 
-print 'Time at KUBRA'
+print 'Time at Keap'
 print weeksString
 print days
